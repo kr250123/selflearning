ce8809244218f227aa56f75fe14990d2505ed635
Krishnaprasad Raghavan <83821338+kr250123@users.noreply.github.com>
2021-10-02 13:57:59 -0400

    Committing Test2 to branch X

---
+++ src/com/authentic/test/AdditionalTestCase.java
@@ -0,0 +1,11 @@
+package com.authentic.test;
+
+public class AdditionalTestCase {
+  public static void main(String[] args) {
+	new AdditionalTestCase();
+  }
+  
+  public AdditionalTestCase() {
+	  System.out.println("Testing Git ");
+  }
+}
