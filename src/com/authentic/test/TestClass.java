434f4e13bda07331c9d5773683e5d228710559b7
Krishnaprasad Raghavan <83821338+kr250123@users.noreply.github.com>
2021-10-02 12:28:58 -0400

    First Commit

---
+++ src/com/authentic/test/TestClass.java
@@ -0,0 +1,12 @@
+package com.authentic.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		new TestClass();
+	}
+	
+	public TestClass() {
+		System.out.println("Testing Git Commands...");
+	}
+}
